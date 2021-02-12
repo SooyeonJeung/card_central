c05780e99da1a1be1f29f94ab1ebcb0a0062c220
SooyeonJeung <jeungsooyeon@gmail.com>
2021-02-12 11:12:09 +1100

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :listings
-  root 'listings#index'
+  root "listings#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
